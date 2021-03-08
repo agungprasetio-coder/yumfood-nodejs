48e05f78c156b9dce28ead912db5fcdbb8a5ef6d
Agung Adi Prasetio <prasetioxagung@gmail.com>
2021-03-09 06:58:19 +0700

    handle method at VendorController

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const app = express()
 const PORT = 3000
 const router = require('./routes')
 
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
 app.use(router)
 
 app.listen(PORT, () => {
--- controllers/VendorController.js
+++ controllers/VendorController.js
@@ -1,22 +1,78 @@
+const { Vendor, Tag } = require('../models')
 class Controller {
   static index (req, res, next) {
-    res.send('show all vendor')
+    Vendor.findAll({
+      order: [['name', 'ASC']],
+      include: [Tag]
+    })
+    .then(data => {
+      if (data.length === 0) {
+        res.status(200).json({message: 'there is no restaurant registered yet', status: 200})
+      }
+      res.status(200).json(data)
+    })
+    .catch(err => {
+      next(err)
+    })
   }
 
   static show (req, res, next) {
-    res.send('show vendor by id')
+    Vendor.findByPk(req.params.id)
+    .then(data => {
+      if (!data) {
+        throw new Error({message: 'No Data!', status: 404})
+      }
+      res.status(200).json(data)
+    })
+    .catch(err => {
+      next(err)
+    })
   }
 
   static store (req, res, next) {
-    res.send('add new vendor')
+    const { name, logo } = req.body
+    Vendor.create({
+      name, logo
+    })
+    .then(data => {
+      res.status(201).json(data)
+    })
+    .catch(err => {
+      next(err)
+    })
   }
 
   static update (req, res, next) {
-    res.send('update vendor by id')
+    const { name, logo } = req.body
+    const { id } = req.params
+    Vendor.update({
+      name, logo
+    }, {
+      where: {id}, returning: true
+    })
+    .then(data => {
+      if (!data[0]) {
+        throw new Error({message: 'No Data!', status: 404})
+      }
+      res.status(200).json(data)
+    })
+    .catch(err => {
+      next(err)
+    })
   }
   
   static destroy (req, res, next) {
-    res.send('delete vendor by id')
+    const { id } = req.params
+    Vendor.destroy({ where: { id } })
+    .then(data => {
+      if(!data) {
+        throw new Error({message: 'No Data!', status: 404})
+      }
+      res.status(200).json(data)
+    })
+    .catch(err => {
+      next(err)
+    })
   }
 }
 
